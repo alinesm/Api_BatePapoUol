27b26fb1d4c8ab52c5697723f8708f6977302558
Aline Moura <alinesouzamoura_@hotmail.com>
2023-01-13 12:39:40 -0300

    get messages with conditions

--- src/app.js
+++ src/app.js
@@ -68,25 +68,27 @@ server.get("/participants", async (req, res) => {
 
 server.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
-  const from = req.headers.user;
+  const user = req.headers.user;
   const time = dayjs().format("HH:mm:ss");
 
   try {
+    const userExist = await db
+      .collection("participants")
+      .findOne({ name: user });
+
+    if (!userExist) {
+      console.log("nao existe user");
+      return res.status(404);
+    }
+
     await db.collection("messages").insertOne({
       to: to,
       text: text,
       type: type,
-      from: from,
+      from: user,
       time: time,
     });
 
-    // console.log({
-    //   to: to,
-    //   text: text,
-    //   type: type,
-    //   from: from,
-    //   time: time,
-    // });
     res.status(201);
   } catch (err) {
     console.log(err);
@@ -115,6 +117,24 @@ server.post("/status", async (req, res) => {
   }
 });
 
-server.listen(process.env.PORT, () => {
+server.get("/messages", async (req, res) => {
+  const limit = req.query.limit || 0;
+  const user = req.headers.user;
+
+  try {
+    const allowedMessages = await db
+      .collection("messages")
+      .find({ $or: [{ to: user }, { to: "Todos" }, { from: user }] })
+      .sort({ $natural: -1 })
+      .limit(parseInt(limit))
+      .toArray();
+
+    res.send(allowedMessages);
+  } catch (error) {
+    res.status(500).send("Deu zica no servidor de banco de dados");
+  }
+});
+
+server.listen(5001, () => {
   console.log("Servidor funfou de boas!!!");
 });
